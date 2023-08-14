7d7c8a2070ad02add1fd8713354dbc71815a336e
Paul Hollyer <paul@hollyer.me.uk>
2023-08-14 11:54:36 +0100

    try and fix the minimum and maximum window sizes - without success for MacCatalyst

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,21 @@
-﻿using Microsoft.Extensions.Logging;
+﻿// Fix the minimum and maximum window sizes
+// Ref: https://github.com/dotnet/maui/discussions/2370#discussioncomment-6701453
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
+
+#if WINDOWS
+using WinUIEx;
+
+#elif MACCATALYST
+using CoreGraphics;
+using UIKit;
+
+#elif IOS
+#elif ANDROID
+#elif TIZEN
+
+#endif
 
 namespace MauiPlayground;
 
@@ -19,7 +36,37 @@ public static class MauiProgram
 		builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
+        // Needs using Microsoft.Maui.LifecycleEvents for this extension method
+        builder.ConfigureLifecycleEvents(events =>
+        {
+#if WINDOWS
+            // using WinUIEx
+            events.AddWindows(wndLifeCycleBuilder =>
+            {
+                wndLifeCycleBuilder.OnWindowCreated(window =>
+                {
+                    window.CenterOnScreen(1024,768); //uses WinUIEx
+                });
+            });
+
+#elif MACCATALYST            
+            // Using CoreGraphics and UIKit
+            events.AddiOS(wndLifeCycleBuilder =>
+            {
+                wndLifeCycleBuilder.SceneWillConnect((scene, session, options) =>
+                {
+                    if (scene is UIWindowScene { SizeRestrictions: { } } windowScene)
+                    {
+                        windowScene.SizeRestrictions.MaximumSize = new CGSize(1200, 900);
+                        windowScene.SizeRestrictions.MinimumSize = new CGSize(600, 400);
+                    }
+                });
+
+            });
+#endif
+        });
+
+        return builder.Build();
 	}
 }
 
